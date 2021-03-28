a012ed5392c4dc132a7fcbe8e4b45f594df652f0
FrancescoLuzzi <francescoluzzi99@libero.it>
2021-03-28 18:10:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+from ModelWrapper import WrapperSingle,WrapperMultiple
+
+modello=[".\\mpi\\pose_iter_160000.caffemodel",".\\mpi\\pose_deploy_linevec_faster_4_stages.prototxt"]
+#at the moment is only mpi
+
+def main():
+    parser = argparse.ArgumentParser(description='Process images')
+    parser.add_argument("--videoIn","-i",type=str, default="0" ,help="input file")
+    parser.add_argument("--videoOut","-o",type=str,help="file di output opzionale")
+    parser.add_argument("-m",default=False, action="store_true")
+    args=parser.parse_args()
+    if args.videoOut!=None:
+        args.videoOut=".\\"+args.videoOut
+    wrapper=None
+    if args.m:
+        wrapper=WrapperMultiple(args.videoIn,modello[0],modello[1],args.videoOut)
+    else:
+        wrapper=WrapperSingle(args.videoIn,modello[0],modello[1],args.videoOut)
+    wrapper.run_simulation()
+
+if __name__=="__main__":
+    main()
