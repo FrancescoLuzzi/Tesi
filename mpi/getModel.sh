b2e3627ef65d21db7bda609efd626cbf960a78bd
FrancescoLuzzi <francescoluzzi99@libero.it>
2021-04-03 00:23:40 +0200

    getModel cambiato

--- mpi/getModel.sh
+++ mpi/getModel.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 curl -O http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
+
+if [ "$?" -ne "0" ]; then
+    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
+fi
