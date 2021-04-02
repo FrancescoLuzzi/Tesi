15b655799cdbdf0d9a33447b6c2aa20c921acfc7
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2021-04-02 17:36:19 +0200

    Update getModel.sh

--- mpi/getModel.sh
+++ mpi/getModel.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
+#!/bin/sh
+curl -O http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel
