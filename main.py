e4e0f614d1a3ce8c3c3a04bf19c055490d83eea8
FrancescoLuzzi <francescoluzzi99@libero.it>
2021-04-07 15:03:08 +0200

    Merging dei 2 wrappers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from ModelWrapper import WrapperSingle,WrapperMultiple
+from ModelWrapper import Wrapper
 
 modello=[".\\mpi\\pose_iter_160000.caffemodel",".\\mpi\\pose_deploy_linevec_faster_4_stages.prototxt"]
 
@@ -9,10 +9,7 @@ def main():
     parser.add_argument("--videoOut","-o",type=str,help="file di output opzionale")
     parser.add_argument("-m",default=False, action="store_true")
     args=parser.parse_args()
-    if args.m:
-        wrapper=WrapperMultiple(args.videoIn,modello[0],modello[1],args.videoOut)
-    else:
-        wrapper=WrapperSingle(args.videoIn,modello[0],modello[1],args.videoOut)
+    wrapper=Wrapper(args.videoIn,modello[0],modello[1],args.videoOut,args.m)
     wrapper.run_simulation()
 
 if __name__=="__main__":
