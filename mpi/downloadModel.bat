ea838056d8f2ca0ed3fcb5764aa5b3604a3caff5
FrancescoLuzzi <francescoluzzi99@libero.it>
2021-11-06 18:15:34 +0100

    resolved bug

--- mpi/downloadModel.bat
+++ mpi/downloadModel.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin /transfer wcb /priority high http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel %CD%\pose_iter_160000.caffemodel
+bitsadmin /transfer wcb /priority HIGH http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/pose_iter_160000.caffemodel "%CD%\pose_iter_160000.caffemodel"
