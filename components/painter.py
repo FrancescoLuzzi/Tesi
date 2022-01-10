4c01b532b1dff10666cc29c6eda27655e3297384
FrancescoLuzzi <francescoluzzi99@libero.it>
2022-01-10 01:51:43 +0100

    Reverted if statement since [Head,Neck] come first

--- components/painter.py
+++ components/painter.py
@@ -43,24 +43,25 @@ class PrivatePainter(SimplePainter):
         super().paint_frame(frame, detections)
         for person in detections:
             keys = list(person.keys())
-            if keys == [0, 1]:
-                head = person[keys[0]]
-                neck = person[keys[1]]
-                head_x, head_y = head
-                neck_x, neck_y = neck
-                median_x = int(absolute(head_x - neck_x) / 2 + min([head_x, neck_x]))
-                median_y = int(absolute(head_y - neck_y) / 2 + min([head_y, neck_y]))
-                radius = int(
-                    sqrt(power(head_x - neck_x, 2) + power(head_y - neck_y, 2)) * 0.6
-                )
-                cv.circle(
-                    frame,
-                    (median_x, median_y),
-                    radius,
-                    (0, 0, 0),
-                    thickness=-1,
-                    lineType=cv.FILLED,
-                )
+            if keys != [0, 1]:
+                break
+            head = person[keys[0]]
+            neck = person[keys[1]]
+            head_x, head_y = head
+            neck_x, neck_y = neck
+            median_x = int(absolute(head_x - neck_x) / 2 + min([head_x, neck_x]))
+            median_y = int(absolute(head_y - neck_y) / 2 + min([head_y, neck_y]))
+            radius = int(
+                sqrt(power(head_x - neck_x, 2) + power(head_y - neck_y, 2)) * 0.6
+            )
+            cv.circle(
+                frame,
+                (median_x, median_y),
+                radius,
+                (0, 0, 0),
+                thickness=-1,
+                lineType=cv.FILLED,
+            )
 
 
 def painter_factory(private: bool):
