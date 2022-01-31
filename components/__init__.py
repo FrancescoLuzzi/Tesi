624426ec6c5beb93de406f666a820544c7f355d2
FrancescoLuzzi <francescoluzzi99@libero.it>
2022-01-31 23:26:29 +0100

    changed CapToFile to CapToVideo

--- components/__init__.py
+++ components/__init__.py
@@ -1,7 +1,7 @@
 from .models import model_factory
 from .painters import painter_factory
 from .runners import dir_run, file_run, file_run_monitor, webcam_run_monitor
-from .writers import CapToFileWriter, CapToImageWriter
+from .writers import CapToVideoWriter, CapToImageWriter
 from colorama import init
 
 init()
